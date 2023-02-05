00eb95d54b493b87f1ab5cc267cd0084945bcc35
Miroslav <gmpPB@yandex.ru>
2023-02-05 19:13:25 +0300

    lesson(012) add nuvBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,14 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/routers';
 import './styles/index.scss'
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar />
       <AppRouter />
       <button onClick={toggleTheme}>toggle theme</button>
     </div>
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import scss from './Navbar.module.scss'
+
+interface NavbarProps {
+  className?: string,
+}
+
+export const Navbar = (props: NavbarProps) => {
+  const { 
+    className
+  } = props
+
+  return (
+    <div className={classNames(scss.Navbar, {}, [className])}>
+      <div className={scss.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={scss.mainLink}>Главная</AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
+      </div>
+    </div>
+  );
+};
