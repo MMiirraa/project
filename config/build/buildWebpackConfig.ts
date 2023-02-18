71ba5a51cc8f6902ca86bb01ba4ed007088e8356
Miroslav <gmpPB@yandex.ru>
2023-02-18 22:40:02 +0300

    lesson(024) add absolute import for jest

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,24 +6,24 @@ import { buildPlugins } from './buildPlugins';
 import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { mode, paths, isDev } = options;
+  const { mode, paths, isDev } = options;
 
-    return {
-        mode,
-        entry: paths.entry,
-        output: {
-            // создает выходной файл с хэшем
-            filename: '[name].[contenthash].js',
-            path: paths.build,
-            clean: true,
-        },
-        plugins: buildPlugins(options),
-        module: {
-            // обработка файлов типа png и тд
-            rules: buildLoaders(options),
-        },
-        resolve: buildResolvers(options),
-        devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(options) : undefined,
-    };
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      // создает выходной файл с хэшем
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      // обработка файлов типа png и тд
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
 }
--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -15,5 +15,6 @@ export interface BuildOptions {
   mode: BuildMode;
   paths: BuildPaths;
   isDev: boolean;
-  port: number
+  port: number;
+  analyze?: 'server' | 'disabled';
 }
