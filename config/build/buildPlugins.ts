bc72d79f5b074d0d0bbffdd374a4831cbf807154
Miroslav <gmpPB@yandex.ru>
2023-03-06 19:09:13 +0300

    lesson(027): change buildPlugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,25 +6,33 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev, analyze }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-    // для отображения загрузки лоудера - сборки
-        new webpack.ProgressPlugin(),
-        // использует наш html как образец для build
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new ReactRefreshWebpackPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-            analyzerMode: analyze ? 'server' : 'disabled',
-        }),
-    ];
+    const plugins = [
+        // для отображения загрузки лоудера - сборки
+            new webpack.ProgressPlugin(),
+            // использует наш html как образец для build
+            new HtmlWebpackPlugin({
+                template: paths.html,
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            }),
+            new webpack.DefinePlugin({
+                __IS_DEV__: JSON.stringify(isDev),
+            }),
+            new ReactRefreshWebpackPlugin(),
+            
+    ]
+
+    if(isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+                analyzerMode: analyze ? 'server' : 'disabled',
+            })
+        )
+    }
+    
+    return plugins;
 }
