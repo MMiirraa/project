3239842bf5d1a916653c7050612d8565affeb3fc
Miroslav <gmpPB@yandex.ru>
2023-03-06 19:15:19 +0300

    lesson(027): change buildPlugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,38 +1,32 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({ paths, isDev, analyze }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
+        // использует наш html как образец для build
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
         // для отображения загрузки лоудера - сборки
-            new webpack.ProgressPlugin(),
-            // использует наш html как образец для build
-            new HtmlWebpackPlugin({
-                template: paths.html,
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash:8].css',
-                chunkFilename: 'css/[name].[contenthash:8].css',
-            }),
-            new webpack.DefinePlugin({
-                __IS_DEV__: JSON.stringify(isDev),
-            }),
-            new ReactRefreshWebpackPlugin(),
-            
-    ]
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+    ];
 
-    if(isDev) {
-        plugins.push(new webpack.HotModuleReplacementPlugin())
-        plugins.push(
-            new BundleAnalyzerPlugin({
-                openAnalyzer: false,
-                analyzerMode: analyze ? 'server' : 'disabled',
-            })
-        )
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
     }
-    
+
     return plugins;
 }
