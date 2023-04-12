901540e9e681ec333f5386e71bb97ab815bca231
Miroslav <gmpPB@yandex.ru>
2023-04-12 17:02:38 +0300

    add husky pre commit

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -40,7 +40,6 @@ export const Modal = (props: ModalProps) => {
         }
     }, [onClose]);
 
-    // Новые ссылки!!!
     const onKeyDown = useCallback((e: KeyboardEvent) => {
         if (e.key === 'Escape') {
             closeHandler();
