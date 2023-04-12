cfda648475922c36d7d88dfc27183dd8f7a8d22a
Miroslav <gmpPB@yandex.ru>
2023-04-12 13:36:44 +0300

    032 add costume input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,25 +1,14 @@
 import React, { useState } from 'react';
 import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
-    const [value, setValue] = useState('');
-
-    const onChange = (value: string) => {
-        setValue(value);
-    };
 
     return (
         <div>
             {t('Главная страница')}
             <Counter />
-            <Input
-                onChange={onChange}
-                value={value}
-                placeholder=">"
-            />
         </div>
     );
 };
